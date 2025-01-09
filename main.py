a2c21d88292bd080ec5af126c0e5ecce68b288e5
Bianca Lopes <biancalds18@gmail.com>
2025-01-09 13:32:53 -0300

    feat(main.cpp): adds initial main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from scripts.extract import fetch_pokemon_list
+from scripts.extract import fetch_pokemon_list, fetch_pokemon_details
+from scripts.transform import structure_pokemon_data
+from scripts.analyze import generate_type_count
+from scripts.export import save_plot
 
 def main():
-    fetch_pokemon_list()
+    pokemons = fetch_pokemon_list(limit=100, offset=0)
+    pokemons_datas = [fetch_pokemon_details(pokemon["url"]) for pokemon in pokemons]
+    df = structure_pokemon_data(pokemons_datas)
+    type_counts, plot = generate_type_count(df)
+    save_plot(plot, "type_count.png")
 
 if __name__ == "__main__":
     main()
