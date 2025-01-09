d34e61b37f3beda409b82c53187020b8544f7834
Bianca Lopes <biancalds18@gmail.com>
2025-01-09 12:38:28 -0300

    feat(extract.py): adds fetch_pokemon_details function

--- scripts/extract.py
+++ scripts/extract.py
@@ -5,6 +5,13 @@ def fetch_pokemon_list(limit=100, offset=0):
     url = f"https://pokeapi.co/api/v2/pokemon?limit={limit}&offset={offset}"
     response = requests.get(url)
     if response.status_code == 200:
-        print(response.json()["results"])
+        return response.json()["results"]
     else:
-        print(f"API error: {response.status_code}")
+        raise Exception(f"API error: {response.status_code}")
+
+def fetch_pokemon_details(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"API error: {response.status_code}")
