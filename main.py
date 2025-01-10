b90fb8aba61d5cfb34bb887a26e923d807822160
Bianca Lopes <biancalds18@gmail.com>
2025-01-10 13:22:27 -0300

    feat(analyze.py): adds mean graph

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from scripts.extract import fetch_pokemon_list, fetch_pokemon_details
 from scripts.transform import structure_pokemon_data
-from scripts.analyze import generate_type_count
+from scripts.analyze import generate_type_count_bar_graph, calculate_stats_mean_per_type, get_n_best_pokemon_by_experience, generate_mean_per_type_grouped_bars_graph, generate_best_pokemon_by_experience_graph
 from scripts.export import save_plot
+import pandas as pd
 
 def main():
     pokemons = fetch_pokemon_list(limit=100, offset=0)
     pokemons_datas = [fetch_pokemon_details(pokemon["url"]) for pokemon in pokemons]
     df = structure_pokemon_data(pokemons_datas)
-    type_counts, plot = generate_type_count(df)
-    save_plot(plot, "type_count.png")
+    
+    type_count_plot = generate_type_count_bar_graph(df)
+    save_plot(type_count_plot, "type_count.png")
+    
+    best_pokemon_by_experience = get_n_best_pokemon_by_experience(df, 5)
+    best_pokemon_by_experience_plot = generate_best_pokemon_by_experience_graph(best_pokemon_by_experience)
+    save_plot(best_pokemon_by_experience_plot, "best_pokemon_by_experience.png")
+    
+    mean_per_type = calculate_stats_mean_per_type(df)
+    mean_per_type_plot = generate_mean_per_type_grouped_bars_graph(mean_per_type)
+    save_plot(mean_per_type_plot, "mean_per_type.png")
 
 if __name__ == "__main__":
     main()
--- scripts/analyze.py
+++ scripts/analyze.py
@@ -3,13 +3,42 @@
 import matplotlib.pyplot as plt
 import seaborn as sea
 
-def generate_type_count(df):
+def generate_type_count_bar_graph(df):
     type_counts = df.explode("Tipos")["Tipos"].value_counts()
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(12, 6))
     sea.barplot(x=type_counts.index, y=type_counts.values)
     plt.title("Contagem de Pokémon por Tipo")
     plt.xlabel("Tipo")
     plt.ylabel("Contagem")
     plt.xticks(rotation=45)
     plt.tight_layout()
-    return type_counts, plt
+    return plt
+
+def calculate_stats_mean_per_type(df):
+    return df.explode("Tipos").groupby("Tipos")[["Ataque", "Defesa", "HP"]].mean().reset_index()
+
+def generate_mean_per_type_grouped_bars_graph(df):
+    df_long = df.melt(id_vars="Tipos", var_name="Atributo", value_name="Valor")
+
+    plt.figure(figsize=(12, 6))
+    sea.barplot(data=df_long,x="Tipos", y="Valor", hue="Atributo")
+    plt.title("Atributos Médios por Tipo")
+    plt.xlabel("Valor médio")
+    plt.ylabel("Tipos")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    return plt
+
+def get_n_best_pokemon_by_experience(df, top_n):
+    return df.nlargest(top_n, "Experiência")
+
+def generate_best_pokemon_by_experience_graph(df):
+    df_filtered = df[["Nome", "Experiência"]]
+    print(df_filtered)
+    sea.barplot(data=df_filtered, x="Experiência", y="Nome", orient="h")
+    plt.title("Melhores pokémons por Experiência Base")
+    plt.xlabel("Experiência")
+    plt.ylabel("Pokémon")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    return plt    
